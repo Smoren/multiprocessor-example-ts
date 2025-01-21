82cafb3ca6013615f4d82e0810867a9ee70deec7
Smoren <ofigate@gmail.com>
2025-01-21 11:56:59 +0300

    Minor changes.

--- src/index.ts
+++ src/index.ts
@@ -6,14 +6,6 @@ async function main() {
   const pool = new Pool(poolSize);
   const input = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
 
-  const onSuccess = (result: number, input: number, index: number) => {
-    console.log(`Task #${index} result: ${result}`);
-  };
-
-  const onError = (error: string, input: number, index: number) => {
-    console.error(`Task #${index} error: ${error}`);
-  };
-
   const result = await pool.map(input, calcSinTask, {
     onTaskSuccess: (result: number, input: number, index: number) => {
       console.log(`Task #${index} | result: ${result}, input: ${input}`);
